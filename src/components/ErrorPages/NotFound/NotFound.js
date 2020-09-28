56a3b494ee94760900c936adfdb943824365079e
amilkardev <melek.ferhi@gmail.com>
2020-09-28 08:38:05 +0100

    weekend trial

---
+++ src/components/ErrorPages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NotFound.css'
+
+const notFound = (props) => {
+    return (
+        <p className={'notFound'}>
+            "404 Sorry Couldn't find your page !!"
+        </p>
+    )
+}
+export default notFound;
